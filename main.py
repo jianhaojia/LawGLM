d52d153da0e3ab0df65dd0b761f5cdb0a06a978f
jianhaojia <928510924@qq.com>
2024-06-12 23:36:37 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,272 +1,230 @@
-
-import json
-
-from langchain_openai import ChatOpenAI
-from langchain.prompts import (
-    ChatPromptTemplate,
-    MessagesPlaceholder,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from openai import OpenAI
-from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
-from langchain_core.output_parsers import StrOutputParser
-from langchain.output_parsers import JsonOutputToolsParser
-from langchain_core.tools import tool
 from zhipuai import ZhipuAI
+import json
+from baseUrl.offical_base  import search_company_name_by_info,get_company_info,search_company_name_by_register,get_company_register,search_company_name_by_sub_info,get_sub_company_info,search_case_num_by_legal_document,get_legal_document
 
-llm = ChatOpenAI(
-    temperature=0.95,
-    model="glm-4",
-    openai_api_key="bfc9a09dce86070855c637f889604bc8.eFiLKw1ZcQ91pqtn",
-    openai_api_base="https://open.bigmodel.cn/api/paas/v4/"
-)
-from baseUrl.base import get_company_name,get_company_info,search_company_name_by_register,get_company_register,search_company_name_by_sub_info,get_sub_company_info,search_case_num_by_legal_document,get_legal_document
 
-client = ZhipuAI(api_key="bfc9a09dce86070855c637f889604bc8.eFiLKw1ZcQ91pqtn")
-messages = []
-tools=[{
-            "type": "function",
-            "function": {
-                "name": "get_company_info",
-                "description": "用户希望通过这个方法来获得公司基本信息，需要提供公司名字",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "company_name": {
-                            "type": "string",
-                            "description":"公司名字"
-                        }
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_company_info",
+            "description": "根据公司名称获得该公司所有基本信息",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "company_name": {
+                        "description": "公司名称",
+                        "type": "string"
                     }
                 },
-                "required": ["company_name"],
-            }
-        },
-            {
-                "type": "function",
-                "function": {
-                    "name": "get_company_name",
-                    "description": """根据传入的key，value获取公司的名称，user的内容必须包含公司内容的属性名和对应值，且仅仅有获取公司名字的表达，如果是传入公司名字的key，value则不是这个方法""",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "key": {
-                                "type": "string",
-                                "description":"是属性的名称,必须是中文"
-                            },
-                            "value": {
-                                "type": "string",
-                                "description": "是属性的值"
-                            }
-                        },
-                        "required": ["key","value"]
-                    }
-                }
+                "required": [ "company_name" ]
             },
-{
-            "type": "function",
-            "function": {
-                "name": "get_company_register",
-                "description": "用户希望用这个方法来获取公司注册信息，问题需要有注册等类似的词语，且提供公司名字",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "company_name": {
-                            "type": "string",
-                            "description":"公司名字"
-                        }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "search_company_name_by_info",
+            "description": "根据公司基本信息某个字段是某个值来查询具体的公司名称",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "key": {
+                        "description": "公司基本信息的某个属性",
+                        "type": "string"
+                    },
+                    "value": {
+                        "description": "公司基本信息的某个属性的值",
+                        "type": "string"
                     }
                 },
-                "required": ["company_name"],
-            }
-        },
-            {
-                "type": "function",
-                "function": {
-                    "name": "search_company_name_by_register",
-                    "description": """在注册公司信息里面，根据传入的key，value获取公司的名称，user的内容必须包含公司内容的属性名和对应值，且仅仅有获取公司名字的表达，如果是传入公司名字的key，value则不是这个方法""",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "key": {
-                                "type": "string",
-                                "description":"是属性的名称,必须是中文"
-                            },
-                            "value": {
-                                "type": "string",
-                                "description": "是属性的值"
-                            }
-                        },
-                        "required": ["key","value"]
-                    }
-                }
+                "required": [ "key","value" ]
             },
-{
-            "type": "function",
-            "function": {
-                "name": "get_sub_company_info",
-                "description": "关于关联公司的信息，根据公司名字获取关联公司的全部基本信息，user的内容必须包含公司名称，且有获取公司信息的表达",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "company_name": {
-                            "type": "string",
-                            "description":"公司名字"
-                        }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_company_register",
+            "description": "根据公司名称获得该公司所有注册信息",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "company_name": {
+                        "description": "公司名称",
+                        "type": "string"
                     }
                 },
-                "required": ["company_name"],
-            }
-        },
-            {
-                "type": "function",
-                "function": {
-                    "name": "search_company_name_by_sub_info",
-                    "description": """关于关联公司的信息，根据传入的key，value获取关联公司的名称，user的内容必须包含公司内容的属性名和对应值，且仅仅有获取公司名字的表达，如果是传入公司名字的key，value则不是这个方法""",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "key": {
-                                "type": "string",
-                                "description":"是属性的名称,必须是中文"
-                            },
-                            "value": {
-                                "type": "string",
-                                "description": "是属性的值"
-                            }
-                        },
-                        "required": ["key","value"]
-                    }
-                }
+                "required": [ "company_name" ]
             },
-{
-            "type": "function",
-            "function": {
-                "name": "get_legal_document",
-                "description": "根据案号获得该案所有基本信息，如何解析到的case_num没有号结尾，则加个号",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "case_num": {
-                            "type": "string",
-                            "description":"案号"
-                        }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "search_company_name_by_register",
+            "description": "根据公司注册信息某个字段是某个值来查询具体的公司名称",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "key": {
+                        "description": "公司注册信息的某个属性",
+                        "type": "string"
+                    },
+                    "value": {
+                        "description": "公司注册信息的某个属性的值",
+                        "type": "string"
                     }
                 },
-                "required": ["case_num"],
-            }
-        },
-            {
-                "type": "function",
-                "function": {
-                    "name": "search_case_num_by_legal_document",
-                    "description": """根据法律文书某个字段是某个值来查询具体的案号""",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "key": {
-                                "type": "string",
-                                "description":"是属性的名称,必须是中文，不能是案件号"
-                            },
-                            "value": {
-                                "type": "string",
-                                "description": "是属性的值"
-                            }
-                        },
-                        "required": ["key","value"]
+                "required": [ "key","value" ]
+            },
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_sub_company_info",
+            "description": "根据公司名称获得该公司所有关联子公司信息",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "company_name": {
+                        "description": "公司名称",
+                        "type": "string"
                     }
-                }
-            }
-        ]
-def get_completion(messages):
-    response = client.chat.completions.create(
-        model="glm-4",
-        messages=messages,
-        temperature=0.7,
-        tools=tools
-    )
-    return response.choices[0].message
+                },
+                "required": ["company_name"]
+            },
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "search_company_name_by_sub_info",
+            "description": "根据关联子公司信息某个字段是某个值来查询具体的公司名称",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "key": {
+                        "description": "关联子公司信息某个字段",
+                        "type": "string"
+                    },
+                    "value": {
+                        "description": "关联子公司信息某个字段的值",
+                        "type": "string"
+                    }
+                },
+                "required": ["key", "value"]
+            },
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_legal_document",
+            "description": "根据案号获得该案所有基本信息",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "case_num": {
+                        "description": "案号",
+                        "type": "string"
+                    }
+                },
+                "required": ["case_num"]
+            },
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "search_company_name_by_sub_info",
+            "description": "根据法律文书某个字段是某个值来查询具体的案号",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "key": {
+                        "description": "法律文书信息的某个字段",
+                        "type": "string"
+                    },
+                    "value": {
+                        "description": "法律文书信息的某个字段的值",
+                        "type": "string"
+                    }
+                },
+                "required": ["key", "value"]
+            },
+        }
+    }
+]
 
 
+def parse_function_call(input):
+    try:
+        messages = []
+        # messages.append({"role": "system", "content": prompt})
+        messages.append({"role": "system", "content": "你是一个法律咨询机器人，需要通过理解意图，参考例子的格式中找到实体名称和实体值的格式，解析实体名称和实体的值，并从相关方法里面寻找答案，返回答案。"})
+        messages.append({"role": "user", "content": input})
 
-def print_json(data):
-    """
-    打印参数。如果参数是有结构的（如字典或列表），则以格式化的 JSON 形式打印；
-    否则，直接打印该值。
-    """
-    if hasattr(data, 'model_dump_json'):
-        data = json.loads(data.model_dump_json())
-
-    if (isinstance(data, (list))):
-        for item in data:
-            print_json(item)
-    elif (isinstance(data, (dict))):
-        print(json.dumps(
-            data,
-            indent=4,
-            ensure_ascii=False
-        ))
-    else:
-        print(data)
-
-
-
-def parse_function_call(model_response,messages):
-    if model_response.choices[0].message.tool_calls:
-        tool_call = model_response.choices[0].message.tool_calls[0]
-        args = tool_call.function.arguments
-        function_result = {}
-        if tool_call.function.name == "get_company_name":
-            function_result = get_company_name(**json.loads(args))
-        if tool_call.function.name == "get_company_info":
-            function_result = get_company_info(**json.loads(args))
-        if tool_call.function.name == "get_company_register":
-            function_result = get_company_register(**json.loads(args))
-        if tool_call.function.name == "search_company_name_by_register":
-            function_result = search_company_name_by_register(**json.loads(args))
-        if tool_call.function.name == "get_sub_company_info":
-            function_result = get_sub_company_info(**json.loads(args))
-        if tool_call.function.name == "search_company_name_by_sub_info":
-            function_result = search_company_name_by_sub_info(**json.loads(args))
-        if tool_call.function.name == "search_case_num_by_legal_document":
-            function_result = search_case_num_by_legal_document(**json.loads(args))
-        if tool_call.function.name == "get_legal_document":
-            function_result = get_legal_document(**json.loads(args))
-        messages.append({
-            "role": "tool",
-            "content": f"{json.dumps(function_result)}",
-            "tool_call_id":tool_call.id
-        })
         response = client.chat.completions.create(
-            model="glm-4",
+            model="glm-4",  # 填写需要调用的模型名称
             messages=messages,
             tools=tools,
         )
-        print(messages)
-        print(response.choices[0].message.content)
-        messages.append(response.choices[0].message.model_dump())
+        if response.choices[0].message.tool_calls:
+            tool_call = response.choices[0].message.tool_calls[0]
+            args = tool_call.function.arguments
+            function_result = {}
+            if tool_call.function.name == "search_company_name_by_info":
+                function_result = search_company_name_by_info(**json.loads(args))
+            if tool_call.function.name == "get_company_info":
+                function_result = get_company_info(**json.loads(args))
+            if tool_call.function.name == "get_company_register":
+                function_result = get_company_register(**json.loads(args))
+            if tool_call.function.name == "search_company_name_by_register":
+                function_result = search_company_name_by_register(**json.loads(args))
+            if tool_call.function.name == "get_sub_company_info":
+                function_result = get_sub_company_info(**json.loads(args))
+            if tool_call.function.name == "search_company_name_by_sub_info":
+                function_result = search_company_name_by_sub_info(**json.loads(args))
+            if tool_call.function.name == "search_case_num_by_legal_document":
+                function_result = search_case_num_by_legal_document(**json.loads(args))
+            if tool_call.function.name == "get_legal_document":
+                function_result = get_legal_document(**json.loads(args))
+            messages.append({
+                "role": "tool",
+                "content": f"{json.dumps(function_result)}",
+                "tool_call_id":tool_call.id
+            })
+            response = client.chat.completions.create(
+                model="glm-4",
+                messages=messages,
+                tools=tools,
+            )
+            print(response.choices[0].message.content)
+            return response.choices[0].message.content
+    except Exception as e:
+        return '不知道'
+
+# parse_function_call('找下注册号为320512400000458是哪个公司')
+
+#submit
+id=0
+def submit():
+    with open('D:\\pythonctest\\law\\data\\question(1).json', 'r', encoding='utf-8') as input_file:
+        for line in input_file:
+            question_data = json.loads(line)
+            if question_data['id']>=id:
+
+                with open('answers2.json', 'a', encoding='utf-8') as output_file:
+                    print(question_data['id'])
+                    answer = parse_function_call(question_data['question'])
+                    question_data['answer'] = answer
+                    json_str = json.dumps(question_data, ensure_ascii=False, separators=(',', ':'))
+                    output_file.write(json_str + '\n')
 
 
 
-
-prompt = "深圳能源集团股份有限公司为被告的次数是多少次"
-
-
-messages.append({"role": "system", "content": "不要假设或猜测传入函数的参数值。如果用户的描述不明确，请要求用户提供必要信息"})
-messages.append({"role": "user", "content": prompt})
-# llm.invoke("请")
-
-response = client.chat.completions.create(
-    model="glm-4",  # 填写需要调用的模型名称
-    messages=messages,
-    tools=tools,
-)
-print(response.choices[0].message)
-messages.append(response.choices[0].message.model_dump())
-
-parse_function_call(response, messages)
-parse_function_call(response, messages)
 
 
 
